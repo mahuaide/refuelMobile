38a4e22ade5de2cdb33d3bd162442ca8e85ccdfd
mahd <mahd@asiainfo.com>
2019-02-19 11:01:10 +0800

    增加加油记录功能

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -1,30 +1,90 @@
 <template>
   <div class="common-wrapper">
-    {{user}}
+    <div class="user-header">
+      <img src="../../../static/img/default-head.png" alt="">
+    </div>
+    <div class="user-name">{{$store.state.user.userName}}</div>
+    <div class="user-info">
+      <div class="user-mileage">{{$store.state.user.mileage}}公里</div>
+      <div class="user-license">{{$store.state.user.license}}</div>
+      <div class="user-payTotal">{{$store.state.user.payTotal}}元</div>
+    </div>
+    <split></split>
+    <div class="user-info-unit">
+      资料修改
+    </div>
+    <split></split>
+    <div class="user-info-unit">
+      密码重置
+    </div>
+    <split></split>
+    <div class="user-info-unit">
+      当前版本
+    </div>
+    <split></split>
   </div>
 </template>
 
 
 <script type="text/ecmascript-6">
   import {getLoginUserInfo} from '../../http/api'
+  import split from '../common/split.vue'
   export default{
     data(){
-      return {
-          user:{}
-      }
+      return {}
     },
     mounted(){
-      getLoginUserInfo().then(res => {
-        let {code, data, errMsg} = res.data;
-        if (code == 200) {
-          this.user = data[0];
-        }
-      })
+    },
+    components: {
+      split
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+  .user-header
+    img
+      display block
+      width 120px
+      height 120px
+      margin 90px auto 30px auto
+      border-radius 50%
+
+  .user-name
+    width 100%
+    text-align center
+    height 30px
+    line-height 30px
+    font-size 32px
+  .user-info
+    width 100%
+    display flex
+    margin 30px auto 30px auto
+    font-size 28px
+    > div
+     box-sizing border-box
+    .user-mileage
+      flex 1.5
+      padding-right 20px
+      text-align right
+    .user-license
+      flex 1
+      padding 0 10px
+      border-left 2px solid #000
+      border-right 2px solid #000
+      text-align center
+    .user-payTotal
+      flex 1.5
+      padding-left: 20px
+      text-align left
+
+  .user-info-unit
+    box-sizing border-box
+    width 100%
+    height 90px
+    line-height 90px
+    font-size 30px
+    padding-left 20px
 
 </style>
 
